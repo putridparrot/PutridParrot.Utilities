ba0423d88f2e886321ed227cc58e58171775de36
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-07-12 21:14:11 +0100

    Removed Path for now

--- PutridParrot.Utilities/File/Path.cs
+++ PutridParrot.Utilities/File/Path.cs
@@ -1,78 +1,78 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿//using System.Collections.Generic;
+//using System.Text;
 
-namespace PutridParrot.Utilities
-{
-    /// <summary>
-    /// This object is used to access/generate file path information
-    /// </summary>
-    /// TODO: Check if this code is still required or been superceded in later .NET
-    public static partial class Path
-    {
-        /// <summary>
-        /// Combines a root path with a relative path to give path combined of the two. Note: If the root path
-        /// ends with a seperator token, this is ignored in favour of the relative path, so for example if the
-        /// root was c:\hello\world\ but the relative was ..\you then the combined would be
-        /// c:\hello\you (notice the missing \). 
-        /// </summary>
-        /// <example>
-        /// string root = @"c:\home\sub\one";
-        /// string relative = @"..\\..\\new";
-        /// 
-        /// string combined = Path.Combine(root, relative);
-        /// </example>
-        /// <param name="root">The root path, i.e. the path that the relative combines with</param>
-        /// <param name="relative">The relative path to combine with the root</param>
-        /// <returns>The combination of the two paths</returns>
-        public static string Combine(string root, string relative)
-        {
-            // removes any prefixes seperator from the relative and suffixed seperator from the root
-            string normalizedRoot = NormalizePath(root).TrimEnd(System.IO.Path.DirectorySeparatorChar);
-            string normalizedRelative = NormalizePath(relative);
+//namespace PutridParrot.Utilities
+//{
+//    /// <summary>
+//    /// This object is used to access/generate file path information
+//    /// </summary>
+//    /// TODO: Check if this code is still required or been superceded in later .NET
+//    public static partial class Path
+//    {
+//        /// <summary>
+//        /// Combines a root path with a relative path to give path combined of the two. Note: If the root path
+//        /// ends with a seperator token, this is ignored in favour of the relative path, so for example if the
+//        /// root was c:\hello\world\ but the relative was ..\you then the combined would be
+//        /// c:\hello\you (notice the missing \). 
+//        /// </summary>
+//        /// <example>
+//        /// string root = @"c:\home\sub\one";
+//        /// string relative = @"..\\..\\new";
+//        /// 
+//        /// string combined = Path.Combine(root, relative);
+//        /// </example>
+//        /// <param name="root">The root path, i.e. the path that the relative combines with</param>
+//        /// <param name="relative">The relative path to combine with the root</param>
+//        /// <returns>The combination of the two paths</returns>
+//        public static string Combine(string root, string relative)
+//        {
+//            // removes any prefixes seperator from the relative and suffixed seperator from the root
+//            string normalizedRoot = NormalizePath(root).TrimEnd(System.IO.Path.DirectorySeparatorChar);
+//            string normalizedRelative = NormalizePath(relative);
 
-            string backToken = "..";
+//            string backToken = "..";
 
-            string[] rootSplit = normalizedRoot.Split(System.IO.Path.DirectorySeparatorChar);
-            string[] relativeSplit = normalizedRelative.Split(System.IO.Path.DirectorySeparatorChar);
+//            string[] rootSplit = normalizedRoot.Split(System.IO.Path.DirectorySeparatorChar);
+//            string[] relativeSplit = normalizedRelative.Split(System.IO.Path.DirectorySeparatorChar);
 
-            Stack<string> stack = new Stack<string>();
-            foreach (string rootSeperate in rootSplit)
-            {
-                stack.Push(rootSeperate);
-            }
+//            Stack<string> stack = new Stack<string>();
+//            foreach (string rootSeperate in rootSplit)
+//            {
+//                stack.Push(rootSeperate);
+//            }
 
-            foreach (string relativeSeperate in relativeSplit)
-            {
-                if (relativeSeperate == backToken)
-                {
-                    if (stack.Count > 0)
-                    {
-                        stack.Pop();
-                    }
-                }
-                else
-                {
-                    stack.Push(relativeSeperate);
-                }
-            }
+//            foreach (string relativeSeperate in relativeSplit)
+//            {
+//                if (relativeSeperate == backToken)
+//                {
+//                    if (stack.Count > 0)
+//                    {
+//                        stack.Pop();
+//                    }
+//                }
+//                else
+//                {
+//                    stack.Push(relativeSeperate);
+//                }
+//            }
 
-            // recombine with seperators
-            StringBuilder combined = new StringBuilder();
-            string[] splitStack = stack.ToArray();
-            for (int i = splitStack.Length - 1; i >= 0; i--)
-            {
-                combined.Append(splitStack[i]);
-                if (i > 0)
-                {
-                    combined.Append(System.IO.Path.DirectorySeparatorChar);
-                }
-            }
-            return combined.ToString();
-        }
+//            // recombine with seperators
+//            StringBuilder combined = new StringBuilder();
+//            string[] splitStack = stack.ToArray();
+//            for (int i = splitStack.Length - 1; i >= 0; i--)
+//            {
+//                combined.Append(splitStack[i]);
+//                if (i > 0)
+//                {
+//                    combined.Append(System.IO.Path.DirectorySeparatorChar);
+//                }
+//            }
+//            return combined.ToString();
+//        }
 
-        private static string NormalizePath(string path)
-        {
-            return path.Trim().TrimStart(System.IO.Path.DirectorySeparatorChar);
-        }
-    }
-}
+//        private static string NormalizePath(string path)
+//        {
+//            return path.Trim().TrimStart(System.IO.Path.DirectorySeparatorChar);
+//        }
+//    }
+//}
--- Tests.PutridParrot.Utilities/PathTests.cs
+++ Tests.PutridParrot.Utilities/PathTests.cs
@@ -1,146 +1,146 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using NUnit.Framework;
-using PutridParrot.Utilities;
+﻿//using System.Diagnostics.CodeAnalysis;
+//using NUnit.Framework;
+//using PutridParrot.Utilities;
 
-namespace Tests.PutridParrot.Utilities
-{
-    [ExcludeFromCodeCoverage]
-    [TestFixture]
-    public class PathTests
-    {
-        [Test]
-        public void Combine1()
-        {
-            var root = "c:\\home\\sub\\one";
-            var relative = "..\\two";
+//namespace Tests.PutridParrot.Utilities
+//{
+//    [ExcludeFromCodeCoverage]
+//    [TestFixture]
+//    public class PathTests
+//    {
+//        [Test]
+//        public void Combine1()
+//        {
+//            var root = "c:\\home\\sub\\one";
+//            var relative = "..\\two";
 
-            Assert.AreEqual("c:\\home\\sub\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineNoBacks()
-        {
-            var root = "c:\\home\\sub\\one";
-            var relative = "two";
+//        [Test]
+//        public void CombineNoBacks()
+//        {
+//            var root = "c:\\home\\sub\\one";
+//            var relative = "two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineNoBacksPreceedingSeperator()
-        {
-            var root = "c:\\home\\sub\\one";
-            var relative = "\\two";
+//        [Test]
+//        public void CombineNoBacksPreceedingSeperator()
+//        {
+//            var root = "c:\\home\\sub\\one";
+//            var relative = "\\two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineNoBacksPostSeperator()
-        {
-            var root = "c:\\home\\sub\\one";
-            var relative = "two\\";
+//        [Test]
+//        public void CombineNoBacksPostSeperator()
+//        {
+//            var root = "c:\\home\\sub\\one";
+//            var relative = "two\\";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two\\", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two\\", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineSuffixedRoot()
-        {
-            var root = "c:\\home\\sub\\one\\";
-            var relative = "two";
+//        [Test]
+//        public void CombineSuffixedRoot()
+//        {
+//            var root = "c:\\home\\sub\\one\\";
+//            var relative = "two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineLeadingWhitespaceRelative()
-        {
-            var root = "c:\\home\\sub\\one\\";
-            var relative = "    two";
+//        [Test]
+//        public void CombineLeadingWhitespaceRelative()
+//        {
+//            var root = "c:\\home\\sub\\one\\";
+//            var relative = "    two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineTrailingWhitespaceRelative()
-        {
-            var root = "c:\\home\\sub\\one\\";
-            var relative = "two    ";
+//        [Test]
+//        public void CombineTrailingWhitespaceRelative()
+//        {
+//            var root = "c:\\home\\sub\\one\\";
+//            var relative = "two    ";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineLeadingWhitespaceRoot()
-        {
-            var root = "   c:\\home\\sub\\one\\";
-            var relative = "two";
+//        [Test]
+//        public void CombineLeadingWhitespaceRoot()
+//        {
+//            var root = "   c:\\home\\sub\\one\\";
+//            var relative = "two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineTrailingWhitespaceRoot()
-        {
-            var root = "c:\\home\\sub\\one\\    ";
-            var relative = "two";
+//        [Test]
+//        public void CombineTrailingWhitespaceRoot()
+//        {
+//            var root = "c:\\home\\sub\\one\\    ";
+//            var relative = "two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineLeadingWhitespaceBoth()
-        {
-            var root = "   c:\\home\\sub\\one\\";
-            var relative = "    two";
+//        [Test]
+//        public void CombineLeadingWhitespaceBoth()
+//        {
+//            var root = "   c:\\home\\sub\\one\\";
+//            var relative = "    two";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineTrailingWhitespaceBoth()
-        {
-            var root = "c:\\home\\sub\\one\\   ";
-            var relative = "two   ";
+//        [Test]
+//        public void CombineTrailingWhitespaceBoth()
+//        {
+//            var root = "c:\\home\\sub\\one\\   ";
+//            var relative = "two   ";
 
-            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\one\\two", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineStaggeredBackTokens()
-        {
-            var root = "c:\\home\\sub\\one\\";
-            var relative = "..\\two\\..";
+//        [Test]
+//        public void CombineStaggeredBackTokens()
+//        {
+//            var root = "c:\\home\\sub\\one\\";
+//            var relative = "..\\two\\..";
 
-            Assert.AreEqual("c:\\home\\sub", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineSimpleBack()
-        {
-            var root = "c:\\home\\sub\\one\\";
-            var relative = "..";
+//        [Test]
+//        public void CombineSimpleBack()
+//        {
+//            var root = "c:\\home\\sub\\one\\";
+//            var relative = "..";
 
-            Assert.AreEqual("c:\\home\\sub", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineSimpleBackWithTrailing()
-        {
-            var root = "c:\\home\\sub\\one\\";
-            var relative = "..\\";
+//        [Test]
+//        public void CombineSimpleBackWithTrailing()
+//        {
+//            var root = "c:\\home\\sub\\one\\";
+//            var relative = "..\\";
 
-            Assert.AreEqual("c:\\home\\sub\\", Path.Combine(root, relative));
-        }
+//            Assert.AreEqual("c:\\home\\sub\\", Path.Combine(root, relative));
+//        }
 
-        [Test]
-        public void CombineBackTooFar()
-        {
-            var root = "c:\\home";
-            var relative = "..\\..\\..\\home";
+//        [Test]
+//        public void CombineBackTooFar()
+//        {
+//            var root = "c:\\home";
+//            var relative = "..\\..\\..\\home";
 
-            Assert.AreEqual("home", Path.Combine(root, relative));
-        }
-    }
-}
+//            Assert.AreEqual("home", Path.Combine(root, relative));
+//        }
+//    }
+//}
